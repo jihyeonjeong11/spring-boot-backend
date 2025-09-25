7c8b92862abe0f36130b527976a23379240a6ad0
jihyeonjeong11 <wjdwlgus11@gmail.com>
2025-09-25 20:59:49 +0900

    some updates

--- traditional-mybatis/src/main/java/com/example/practice/model/User.java
+++ traditional-mybatis/src/main/java/com/example/practice/model/User.java
@@ -1,5 +1,9 @@
 package com.example.practice.model;
 
+import java.time.LocalDateTime;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,8 +12,14 @@ import lombok.ToString;
 @Setter
 @ToString(exclude = "password")
 public class User {
+   
     private Long id;
+    private String username; // SQL의 username 컬럼과 매핑
+    private String password; // SQL의 password 컬럼과 매핑
     private String name;
     private String email;
-    private String password;
+    private String roles;
+    private boolean enabled = true;
+    private LocalDateTime createdAt; // SQL의 created_at 컬럼과 매핑
+    private LocalDateTime updatedAt;
 }
